9818ecc3d8f4386a37f99884438af7830d567158
Connor McCoy <comccoy@cfl.rr.com>
2025-06-19 15:15:00 -0400

    initial project files

---
+++ README.md
@@ -0,0 +1,10 @@
+# BlitzLog
+Helldivers 2 seed scouter, for people who wanna watch Youtube/drink Pepsi/whatever while a bot navigates HD2 UI for them and generates a collage of all available seed.
+
+See [Setup][1] for how to download, launch, and configure <br>
+then [Guide][2] for how to use <br>
+and maybe [Dev][3] for cryptic personal notes...
+
+[1]: <docs/setup.md>
+[2]: <docs/guide.md>
+[3]: <docs/dev.md>
---
+++ tasks.py
@@ -0,0 +1,29 @@
+from invoke import task
+
+# @task
+# def build(c):
+#     """
+#     Use pyinstaller to build the exe (outputs to 'dist')
+#     """
+#     c.run('pyinstaller --clean BlitzLog.spec')
+
+# @task
+# def run(c):
+#     """
+#     Run the application as a script on host
+#     """
+#     c.run('python -m app.app')
+
+@task
+def restore(c):
+    """
+    Use pip to ensure dependencies are installed
+    """
+    c.run('pip install -r requirements.txt')
+
+@task
+def test(c):
+    """
+    Run all tests in project
+    """
+    c.run('pytest')
